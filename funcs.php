ae3ee4d2eb3ccc97031e966f0d4414652f418d1e
polyzer <realpolyzer@yandex.ru>
2015-01-25 10:49:48 +0500

    Update funcs.php

--- funcs.php
+++ funcs.php
@@ -5,26 +5,80 @@ if ($_SESSION["vk_persontype"]["true_connection"] &&
    ) 
 {
 	$operation = $_POST["operation"];
-	$question_number = $_POST["question_number"];
+
 	
-	$mysqli = new mysqli("localhost", "root", "000000", "person_type");
+	$mysqli = new mysqli("localhost", //адрес хоста БД
+						 "root", // имя пользователя
+						 "000000", //пароль
+						 "person_type"); //база данных
+	
 	if ($mysqli->connect_errno) {
 		echo "Не удалось подключиться к MYSQL: (" . $mysqli->connect_errno . ") ". $mysqli->connect_error;
 	} else {
 		$mysqli->set_charset("utf8");
 	}
+	
+	
 	if ($operation === "get_question") {
-		$res = $mysqli->query("SELECT * FROM questions WHERE num=".$question_number);
+		$question_number = $_POST["question_number"];
+		$res = $mysqli->query("SELECT * FROM `questions` WHERE `num`=".$question_number.";");
 		$row = $res->fetch_assoc();
 		$return_string = "";
 		foreach($row as $key => $value) {
 			$return_string .= $key."===".$value."&&&";
 		}
 		echo $return_string;
-	}	
-	if ($operation === "set_test_result") {
-		$res = $mysqli->query("INSERT INTO person_type.");
 	}
+	
+		
+	if ($operation === "save_test_result") {
+		$first_column = $_POST["first_column"];
+		$second_column = $_POST["second_column"];
+		$third_column = $_POST["third_column"];//Сделать преобразование $_POST
+		$fourth_column = $_POST["fourth_column"];
+		$time_date = date("Y-m-d H:i:s");
+		$vk_id = $_POST["vk_id"];
+		$query_string = "INSERT INTO `results` (`result_id`,
+												   `first_column`,
+												   `second_column`,
+												   `third_column`,
+												   `fourth_column`,
+												   `time_date`,
+												   `vk_id`)
+							   VALUES (NULL, 
+							           '$first_column',
+							           '$second_column',
+							           '$third_column',
+							           '$fourth_column',
+							           '$time_date',
+							           '$vk_id');";
+
+		if (!($res = $mysqli->query($query_string)))
+			echo $mysqli->error;
+		else {
+			echo "server_answer===Данные сохранены";
+		}
+	}
+
+	if ($operation === "get_test_result") {
+		$VKID = $_POST["vk_id"];
+		$res = $mysqli->query("SELECT * FROM `results` WHERE `vk_id`='".$VKID."';");
+		if ($res->num_rows != 0){
+			$row = $res->fetch_assoc();
+			$return_string = "";
+			foreach($row as $key => $value) {
+				$return_string .= $key."===".$value."&&&";
+			}
+			$return_string .= "server_answer===YES_DATA&&&";
+			echo $return_string;
+		} else {
+			echo "server_answer===NO_DATA&&&";
+		}
+		
+		
+	}
+	
+	
 
 } else {
 	echo "You have no permission!";
